05d7190084f34bef6a05b6a299cf97dc365bad1e
Zeggriim <lilian.dorazio@hotmail.fr>
2026-05-04 00:03:26 +0200

    chore(analyse): correctif

--- src/Admin/Infrastructure/Http/AdminFiscal/UpsertController.php
+++ src/Admin/Infrastructure/Http/AdminFiscal/UpsertController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 #[Route('/fiscal-config/{year}', name: 'upsert', requirements: ['year' => '\d{4}'], methods: [Request::METHOD_PUT])]
 class UpsertController extends AbstractAdminController
 {
