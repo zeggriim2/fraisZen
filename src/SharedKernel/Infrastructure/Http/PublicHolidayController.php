30a8b58ccce071d486238e808b0e8787b474bf56
Zeggriim <lilian.dorazio@hotmail.fr>
2026-05-04 01:27:34 +0200

    feat(public-holidays): endpoint GET /api/public-holidays/{year}

---
+++ src/SharedKernel/Infrastructure/Http/PublicHolidayController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\SharedKernel\Infrastructure\Http;
+
+use App\SharedKernel\Infrastructure\PublicHoliday\PublicHolidayService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+#[Route('/api/public-holidays/{year}', name: 'public_holidays', requirements: ['year' => '\d{4}'], methods: ['GET'])]
+final class PublicHolidayController extends AbstractController
+{
+    public function __construct(private readonly PublicHolidayService $publicHolidayService)
+    {
+    }
+
+    public function __invoke(int $year): JsonResponse
+    {
+        return $this->json($this->publicHolidayService->forYear($year));
+    }
+}
